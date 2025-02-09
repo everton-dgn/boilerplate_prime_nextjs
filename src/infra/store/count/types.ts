d8bac56ac0250c6e38bcf34bef642aff38ce11de
Éverton Toffanetto <evertondgn@hotmail.com>
2025-02-08 22:14:28 -0300

    feat: define Middleware type for zustand store configuration

--- src/infra/store/count/types.ts
+++ src/infra/store/count/types.ts
@@ -1,5 +1,7 @@
 import type { StateCreator } from 'zustand'
 
+import type { Middleware } from '../types'
+
 export type State = {
   count: number
 }
@@ -13,13 +15,6 @@ type Actions = {
 
 export type Store = State & Actions
 
-export type Slice = StateCreator<
-  Store,
-  [
-    ['zustand/immer', never],
-    ['zustand/persist', unknown],
-    ['zustand/devtools', never]
-  ]
->
+export type Slice = StateCreator<Store, Middleware>
 
 export type SetAction = Parameters<Slice>[0]
---
+++ src/infra/store/types.ts
@@ -0,0 +1,5 @@
+export type Middleware = [
+  ['zustand/immer', never],
+  ['zustand/persist', unknown],
+  ['zustand/devtools', never]
+]
